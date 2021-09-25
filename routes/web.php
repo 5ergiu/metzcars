c4a5d3e13a13a750653f185a20a9ed0a1f9c744c
5ergiu <sergiu.a.bugeac@gmail.com>
2021-09-25 14:14:02 +0300

    added footer

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 # Public routes
 Route::middleware(['locale'])->group(function() {
-    Route::get('/',              [Controllers\HomeController::class, 'index'])->name('app.home');
-    Route::get('/stock',         [Controllers\StockController::class, 'index'])->name('app.stock');
-    Route::get('/locale/{code}', [Controllers\LocaleController::class, 'handleLocaleChange'])->name('app.locale');
+    Route::get('/',                     [Controllers\HomeController::class, 'index'])->name('app.home');
+    Route::get('/stock',                [Controllers\StockController::class, 'index'])->name('app.stock');
+    Route::get('/locale/{code}',        [Controllers\LocaleController::class, 'handleLocaleChange'])->name('app.locale');
+    Route::get('/terms-and-conditions', [Controllers\LegalController::class, 'terms'])->name('app.legal.terms');
+    Route::get('/privacy-policy',       [Controllers\LegalController::class, 'privacy'])->name('app.legal.privacy');
+    Route::get('/contact',              [Controllers\ContactController::class, 'index'])->name('app.contact');
 });
