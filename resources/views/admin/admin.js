48b2da108b5395f906b3a2260755e2c1d48e2c33
5ergiu <sergiu.a.bugeac@gmail.com>
2021-10-18 22:41:58 +0300

    added gtag js

--- resources/views/admin/admin.js
+++ resources/views/admin/admin.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
     const adminDropdownMenu = document.getElementById('adminDropdownMenu')
 
-    adminDropdownMenu.addEventListener('click', e => {
+    adminDropdownMenu?.addEventListener('click', e => {
         e.stopPropagation()
         document.getElementById('adminUpdateStock').submit()
     })
